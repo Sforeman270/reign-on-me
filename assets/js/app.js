2a6783f45283ee27ebea5e98e801e9ca7f41662a
Sforeman270 <65334884+Sforeman270@users.noreply.github.com>
2020-08-12 14:57:56 -0500

    bugs

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,14 @@
-// variables
 
 
-var todaysDate = moment().format('');
-var listCity = [];
-var cityContainerEl = $("#city-container");
-var enteredCity = $("#enteredCity")
-var temp = $("#temp");
-var uvi = $("#uvi");
-var searchBtn = $("#searchBtn")
-var lastCity = "";
 
-
+const todaysDate = moment().format('');
+const listCity = [];
+const cityContainerEl = $("#cityContainer");
+const enteredCity = $("#enteredCity")
+const temp = $("#temp");
+const uvi = $("#uv");
+const searchBtn = $("#searchBtn")
+let lastCity = "";
 const currentCity = {
     name: '',
     date: '',
@@ -23,17 +21,16 @@ const currentCity = {
 
 
 
-// ajax call to get today's weather by cityname,lat,long
 function populateCurrentCity(currentCity) {
     $.ajax({
-        url: 'http://api.openweathermap.org/data/2.5/weather?q=' + currentCity + '&appid=762a77dec0a17de9f05c06c1b4ac67f4',
+        url: 'http://api.openweathermap.org/data/2.5/weather?q=${enteredCity}&appid=${99c551b81296e62a1c47edacba59e365',
         method: "GET"
     })
         .then(function (response) {
             console.log(response);
             let imageIcon = response.weather
             let imageUrl = "https://openweathermap.org/img/w/" + imageIcon + ".png";
-
+            /*
             $("#weatherImage").attr("src", imageUrl);
 
             $("#enteredCity").html(response.name + " (' todaysDate +')");
@@ -41,20 +38,28 @@ function populateCurrentCity(currentCity) {
             $("#humidity").text("Humidity: " + response.main.humidity);
             $("#windspeed").text("Windspeed:" + response.wind.speed);
             $(".today-header").text(currentCity);
-
+*/
             let latittude = response.coord.lat;
             let longitude = repsonse.coord.lon;
 
 
-            // ajax call to get uv index
+
+            
             $.ajax({
-                url: 'http://api.openweathermap.org/data/2.5/uvi?&appid=762a77dec0a17de9f05c06c1b4ac67f4=' + latittude + '&lon=' + longitude,
+                url: 'https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=minutely&appid=${99c551b81296e62a1c47edacba59e365',
                 method: "GET"
             })
                 .then(function (response) {
                     console.log(response);
+                    $(".current-header").text(currentCity);
+                    $("#enteredCity").html(response.name + response.current.dt);
+                    $("#temperature").text("Temperature:" + response.current.temp);
+                    $("#weatherImage").attr("src", response.current.weather.icon);
+                    $("#humidity").text("Humidity:" + repsonse.current.humidity);
+                    $("#windspeed").text("Windspeed:" + response.wind.speed);
+                    $("#uv").text("UVIndex:" + response.current.uvi);
 
-                    let uvIndex = response.value;
+                    let uvIndex = response.current.uvi;
 
                     $("#uv").append(`uv index: <span class="uvspan"> ${uvIndex}</span>`);
 
@@ -66,67 +71,76 @@ function populateCurrentCity(currentCity) {
                     }
                     else {
                         $(".uvspan").addClass("bad");
+
+                        $enteredCity.val((localStorage.getItem(city)));
                     }
 
 
+                    then(function (response) {
+                        
+                        console.log(response);
+                        let dayOneImg = response.daily[0];
+                
+                        let dayOneImgUrl = "https://openweathermap.org/img/w/" + dateOneImage + ".png";
+                
+                        let dayTwoImg = response.daily[1];
+                
+                        let dayTwoImgUrl = "https://openweathermap.org/img/w/" + dateTwoImage + ".png";
+                
+                        let dayThreeImg = daily[2];
+                
+                        let dayThreeImgUrl = "https://openweathermap.org/img/w/" + dateThreeImage + ".png";
+                
+                        let dayFourImg = response.daily[3];
+                
+                        let dayFourImgUrl = "https://openweathermap.org/img/w/" + dateFourImage + ".png";
+                
+                        let dayFiveImg = response.daily[4];
+                
+                        let dayFiveImgUrl = "https://openweathermap.org/img/w/" + dateFiveImage + ".png";
+                    });
+                    
 
 
 
 
-                    $.ajax({
-                        url: 'http://api.openweathermap.org/data/2.5/forecast?q=chicago&appid=762a77dec0a17de9f05c06c1b4ac67f4',
-                        method: "GET"
-                    })
-                        .then(function (response) {
-                            localStorage.setItem("listCity", cityName);
-                            console.log(response);
-                            let dayOneImg = response.list[5].weather[0].icon;
-
-                            let dayOneImgUrl = "https://openweathermap.org/img/w/" + dateOneImage + ".png";
-
-                            let dayTwoImg = response.list[13].weather[0].icon;
-
-                            let dayTwoImgUrl = "https://openweathermap.org/img/w/" + dateTwomage + ".png";
-
-                            let dayThreeImg = response.list[21].weather[0].icon;
-
-                            let dayThreeImgUrl = "https://openweathermap.org/img/w/" + dateThreeImage + ".png";
-
-                            let dayFourImg = response.list[29].weather[0].icon;
-
-                            let dayFourImgUrl = "https://openweathermap.org/img/w/" + dateFourImage + ".png";
-
-                            let dayFiveImg = response.list[37].weather[0].icon;
-
-                            let dayFiveImgUrl = "https://openweathermap.org/img/w/" + dateFiveImage + ".png";
-
-
-
-                            /*let fiveDay = response.value;
+    
+                    $searchBtn.append($magnifying-glassIcon);
+                    $buttonDiv.append($searchBtn);
+                    $listGroupDiv.append($list-group-history, $enteredCity, $buttonDiv);
+                    $inputForm.append($formGroupDiv);
+                
         
-                            $("#fiveDay").append(`Five Day Forecast: <span class="fiveday"> ${fiveDay}</span>`);
-        
-                            $(".today-header").text('todaysDate +');
-                            $("#enteredCity").html(response.name + " (' todaysDate +')");
-                            let imageIcon = response.weather
-                            let imageUrl = "https://openweathermap.org/img/w/" + imageIcon + ".png";
-                            $("#fweatherImage").attr("src", imageUrl);
-        
-                            $("#ftemperature").text("Temperature: " + response.main.temp);
-                            $("#fhumidity").text("Humidity: " + response.main.humidity);
-        
-        
-        
-                            let latittude = response.coord.latittude;
-                            let longitude = repsonse.coord.longe;
-                            */
-                        })
+                    $(document).on("click", ".searchBtn", function(e) {
+                        e.preventDefault();
+                        console.log("city: ", enteredCity.val());
+                        searchCity();
+                        listCity.push(enteredCity.val());
+                        cityContainerEl.empty();
+                        for (let city of listCity) {
+                            cityContainerEl.append(`
+                                <li class="cityRecord">${city}</li>
+                            `)
+                        }
+                        localStorage.setItem(city, enteredCity.val());
+                        console.log(enteredCity.val());
+
+
+
+
+
+
+
+
+
+
+
+
                 })
         })
 
 
-
-
-}
-populateCurrentCity("chicago");
-
+            
+            
+})}
+populateCurrentCity(enteredCity);
